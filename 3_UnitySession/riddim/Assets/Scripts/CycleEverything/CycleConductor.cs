b75e0d1e3ad240460b66cb14767c29d8aaf65fba
Ellen Lo <lowing@bu.edu>
2023-07-31 11:06:18 -0700

    health bar andd start game button

--- 3_UnitySession/riddim/Assets/Scripts/CycleEverything/CycleConductor.cs
+++ 3_UnitySession/riddim/Assets/Scripts/CycleEverything/CycleConductor.cs
@@ -24,17 +24,24 @@ public class CycleConductor : MonoBehaviour
     float dspSongTime;
     AudioSource musicSource;
     float clipLength;
+    bool gameStarted = false;
 
     float [] notes;
     int nextIndex;
 
-    [SerializeField]
-    Transform notesContainer;
-    
+    // Notes in scene
+    public Transform notesContainer;
     public GameObject notePrefab;
 
+    // Effects
     public Transform effectTransform;
     public GameObject earlyEffect, greatEffect, perfectEffect, lateEffect, missEffect;
+
+    // Health and damage
+    public int maxHealth = 100;
+    private int currentHealth;
+    public HealthBar healthBar;
+    public int missNoteDamage = 2;
 
     void Awake()
     {
@@ -52,27 +59,41 @@ public class CycleConductor : MonoBehaviour
         musicSource = GetComponent<AudioSource>();
         clipLength = musicSource.clip.length;
         secPerBeat = 60f / songBpm;
-        dspSongTime = (float)AudioSettings.dspTime;
-        musicSource.Play();
-        nextIndex = 0;
-        notes = new float[(int)Mathf.Floor(clipLength)];
-
-        for(int i = 0; i < Mathf.Floor(clipLength); i++)
-        {
-            notes[i] = (float) i;
-        }
     }
 
     void Update()
     {
-        songPosition = (float)(AudioSettings.dspTime - dspSongTime - firstBeatOffset);
-        songPositionInBeats = songPosition / secPerBeat;
-        
-        if(nextIndex < notes.Length && notes[nextIndex] < (songPositionInBeats + beatsShownInAdvance))
+        if(gameStarted)
         {
-            SpawnCycleNotes(notes[nextIndex]);
-            nextIndex++;
+            songPosition = (float)(AudioSettings.dspTime - dspSongTime - firstBeatOffset);
+            songPositionInBeats = songPosition / secPerBeat;
+            
+            if(nextIndex < notes.Length && notes[nextIndex] < (songPositionInBeats + beatsShownInAdvance))
+            {
+                SpawnCycleNotes(notes[nextIndex]);
+                nextIndex++;
+            }
         }
+    }
+
+    public void StartNewGame()
+    {
+        // Audio source things
+        dspSongTime = (float)AudioSettings.dspTime;
+        musicSource.Play();
+
+        // Initializing notes data structure
+        nextIndex = 0;
+        notes = new float[(int)Mathf.Floor(clipLength)];
+        for(int i = 0; i < Mathf.Floor(clipLength); i++)
+        {
+            notes[i] = (float) i;
+        }
+
+        // Initializes player health
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+        gameStarted = true;
     }
 
     void SpawnCycleNotes(float beatPosition)
@@ -105,7 +126,7 @@ public class CycleConductor : MonoBehaviour
     { 
         Instantiate(missEffect, effectTransform);
         
-        // currentHealth -= missNoteDamage;
-        // healthBar.SetHealth(currentHealth);
+        currentHealth -= missNoteDamage;
+        healthBar.SetHealth(currentHealth);
     }
 }
